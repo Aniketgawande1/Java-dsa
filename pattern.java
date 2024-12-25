b694c46aaa5479c8eac6189ea32c085976f27898
Aniket Gawande <146917796+codeaniket2005@users.noreply.github.com>
2024-12-26 02:03:49 +0530

    new pattern add

--- pattern.java
+++ pattern.java
@@ -1,13 +1,22 @@
 public class pattern {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 205; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+        // for (int i = 1; i <= 205; i++) {
+        //     for (int j = 0; j <= i; j++) {
+        //         System.out.print("*");
                 
+        //     }
+        //     // System.out.println();
+        int n = 7;
+        for (int i = 1; i<=n; i++) {
+            for (int j =1; j <=n-i+1; j++) {
+                System.out.print("*");
             }
             System.out.println();
+        }
+
+
             
         }
+        
     }
-}
