f37c298dfe9de44e22245789d08a408df34eb388
Aniket Gawande <146917796+codeaniket2005@users.noreply.github.com>
2024-12-29 04:28:55 +0530

    patter-3-complet

--- pattern.java
+++ pattern.java
@@ -54,11 +54,49 @@ public class pattern {
         // }
 
     }
+    public static void butterfly_pattern(int n){
+        //1st half
+        for (int i = 1; i <=n; i++) {
+            //stars - i
+            for (int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+            //spaces - 2*(n-1)
+            for (int j = 1; j <= 2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+
+        // 2nd half
+
+        for (int i = n; i >=1; i--) {
+            for (int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+            //spaces - 2*(n-1)
+            for (int j = 1; j <= 2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+
+    }
 
     public static void main(String[] args) {
         hollow_rectangle(5, 5);
         pyramid_triangle(5);
         inverted_pyramid_triangle(6);
+        butterfly_pattern(5);
+
        
 
             
