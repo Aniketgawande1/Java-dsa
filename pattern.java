cf17935c54fcc35d9809d8a0e0e675b9a6e4acf8
Aniket Gawande <146917796+codeaniket2005@users.noreply.github.com>
2024-12-28 12:27:23 +0530

    pattern completed

--- pattern.java
+++ pattern.java
@@ -1,22 +1,66 @@
 public class pattern {
 
+    public static void hollow_rectangle(int totRows, int totCols){
+        for(int i=1; i<=totRows;i++){
+            for(int j =1; j<=totCols;j++ ){
+                if (i == 1 || i == totRows || j == 1|| j == totCols) {
+                    System.out.print("*");
+                    
+                }else{
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void pyramid_triangle(int n ){
+        for(int i = 1;  i<=n;i++ ){
+            //spaces
+            for(int j = 1; j <= n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        } 
+    }
+
+    public static void inverted_pyramid_triangle(int n ){
+        for(int i = 1;  i<=n;i++ ){
+            //spaces
+            for(int j = 1; j <=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        } 
+    }
+
     public static void main(String[] args) {
+        hollow_rectangle(5, 5);
+        pyramid_triangle(5);
+        inverted_pyramid_triangle(6);
         // for (int i = 1; i <= 205; i++) {
         //     for (int j = 0; j <= i; j++) {
         //         System.out.print("*");
                 
         //     }
         //     // System.out.println();
-        int n = 7;
-        for (int i = 1; i<=n; i++) {
-            for (int j =1; j <=n-i+1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // int n = 7;
+        // for (int i = 1; i<=n; i++) {
+        //     for (int j =1; j <=n-i+1; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
 
 
             
         }
         
     }
+
+    
