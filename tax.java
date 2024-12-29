fcb5ca49dd79e99d47a2c0ca4a52d1e46f23a553
Aniket Gawande <146917796+codeaniket2005@users.noreply.github.com>
2024-12-30 03:49:08 +0530

    change code

--- tax.java
+++ tax.java
@@ -5,10 +5,10 @@ public class tax {
         Scanner sc = new Scanner(System.in);
         int income = sc.nextInt();
         int tax;
-        if(income < 500000) {
+        if(income < 300000) {
             tax = 0;
         } 
-        else if(income>= 500000 && income < 100000){
+        else if(income>= 300000 && income < 100000){
             tax = (int)(income * 0.2);
 
         }
